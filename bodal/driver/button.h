d12430f3169d4ba03912bad0b3638d3bfee656e8
Kei <kkw@robotis.com>
2020-02-29 20:24:04 +0900

    Add/Delete APIs for button.

--- bodal/driver/button.h
+++ bodal/driver/button.h
@@ -16,9 +16,7 @@
 #define  BODAL_BUTTON_MAX_CH    HW_BUTTON_MAX_CH
 
 bool     buttonInit(void);
-
-//bool     buttonAddChannel(uint8_t channel, uint8_t gpio_ch, bool on_state);
-
+void     buttonEnable(bool enable);
 void     buttonResetTime(uint8_t channel);
 bool     buttonGetPressed(uint8_t channel);
 bool     buttonGetPressedEvent(uint8_t channel);
@@ -27,6 +25,8 @@ uint32_t buttonGetPressedTime(uint8_t channel);
 bool     buttonGetReleased(uint8_t channel);
 bool     buttonGetReleasedEvent(uint8_t channel);
 uint32_t buttonGetReleasedTime(uint8_t channel);
+
+bool     buttonOsdGetPressed(uint8_t ch);
 
 #endif  /* BOSDK_ENABLE_BODAL_DRIVER_BUTTON */
 
